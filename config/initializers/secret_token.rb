29114feeb7ac157bb7574849917916704463f3f2
msajnog <msajnog@ubuntu.(none)>
2013-05-18 19:41:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kulinarny::Application.config.secret_key_base = '3eff110408891dcc16e7b0c17768bf4e5b626e29430dfaa5ed68de0ea3fde8f5299ecd0e0bf59d839852d75216cda8c90cacb03cd5f634689b19f5e5b9770db2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kulinarny::Application.config.session_store :encrypted_cookie_store, key: '_kulinarny_session'
